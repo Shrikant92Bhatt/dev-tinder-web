4492cdc843b7e14b445c57f29c09bb952095bd7e
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2025-07-19 19:28:44 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
 import Navbar from "./navbar/Navbar"
+import Body from "./Body"
+import Login from "./Login"
+import About from "./About"
 
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}> 
+          <Route path="/login" element={<Login />} />
+          <Route path="/About" element={<About/>} />
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
